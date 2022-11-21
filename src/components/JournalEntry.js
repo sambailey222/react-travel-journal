5aa31f10088bb017dc914a30221816ebbdc77b8e
Sam <sam.bailey@live.co.uk>
2022-11-21 09:02:26 +0000

    begin building nav

---
+++ src/components/JournalEntry.js
@@ -0,0 +1,7 @@
+export default function JournalEntry() {
+    return (
+    <nav>
+        <h1>Journal Entry</h1>
+    </nav>
+    );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import world from '../world.png';
+
+export default function Navbar() {
+    return (
+    <nav className="navbar">
+            <img className="nav--image"src={world} alt='world-image'/>
+            <h1 className="nav--title">my travel journal.</h1>
+    </nav>
+    );
+}
